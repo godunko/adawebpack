c9aa9d47fb4900cea794e91ec2e0206ce1fa0061
Vadim Godunko <vgodunko@gmail.com>
2020-01-10 14:51:56 +0300

    Primitive memory allocation.

--- source/rtl/s-memory__wasm.adb
+++ source/rtl/s-memory__wasm.adb
@@ -29,28 +29,28 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  Simple implementation for use with ZFP
+--  Simple implementation for use with LLVM/WASM
 
 pragma Restrictions (No_Elaboration_Code);
 --  This unit may be linked without being with'ed, so we need to ensure
 --  there is no elaboration code (since this code might not be executed).
 
---  with System.Storage_Elements;
+with System.Storage_Elements;
 
 package body System.Memory is
---   use System.Storage_Elements;
+   use System.Storage_Elements;
 
---   Heap_Start : Character;
---   for Heap_Start'Alignment use Standard'Maximum_Alignment;
---   pragma Import (C, Heap_Start, "__heap_start");
---   --  The address of the variable is the start of the heap
---
+   Heap_Base : Character;
+   for Heap_Base'Alignment use Standard'Maximum_Alignment;
+   pragma Import (C, Heap_Base, "__heap_base");
+   --  The address of the variable is the start of the heap
+
 --   Heap_End : Character;
 --   pragma Import (C, Heap_End, "__heap_end");
 --   --  The address of the variable is the end of the heap
---
---   Top : aliased Address := Heap_Start'Address;
---   --  First not used address (always aligned to the maximum alignment).
+
+   Top : aliased Address := Heap_Base'Address;
+   --  First not used address (always aligned to the maximum alignment).
 
    ----------------
    -- For C code --
@@ -70,48 +70,50 @@ package body System.Memory is
    -----------
 
    function Alloc (Size : size_t) return System.Address is
-      pragma Unreferenced (Size);
---      Max_Align : constant := Standard'Maximum_Alignment;
---      Max_Size  : Storage_Count;
---      Res       : Address;
+      Max_Align : constant := Standard'Maximum_Alignment;
+      Max_Size  : Storage_Count;
+      Res       : Address;
 
    begin
---      if Size = 0 then
+      if Size = 0 then
 
          --  Change size from zero to nonzero. We still want a proper pointer
          --  for the zero case because pointers to zero-length objects have to
          --  be distinct.
 
---         Max_Size := Max_Align;
---
---      else
+         Max_Size := Max_Align;
+
+      else
          --  Detect overflow in the addition below. Note that we know that
          --  upper bound of size_t is bigger than the upper bound of
          --  Storage_Count.
 
---         if Size > size_t (Storage_Count'Last - Max_Align) then
---            raise Storage_Error;
---         end if;
---
---         --  Compute aligned size
---
---         Max_Size :=
---           ((Storage_Count (Size) + Max_Align - 1) / Max_Align) * Max_Align;
---      end if;
---
---      Res := Top;
+         if Size > size_t (Storage_Count'Last - Max_Align) then
+            raise Storage_Error;
+         end if;
+
+         --  Compute aligned size
+
+         Max_Size :=
+           ((Storage_Count (Size) + Max_Align - 1) / Max_Align) * Max_Align;
+      end if;
+
+      Res := Top;
 
       --  Detect too large allocation
+
+      --  XXX Code is commented out because amount of memory is configurable
+      --  during linking/instantiation and can grow on request. This code
+      --  need to be revisited.
 
 --      if Max_Size >= Storage_Count (Heap_End'Address - Res) then
 --         raise Storage_Error;
 --      end if;
 
       --  Update the top of the heap.
---      Top := Res + Max_Size;
---
---      return Res;
-      return System.Null_Address;
+      Top := Res + Max_Size;
+
+      return Res;
    end Alloc;
 
    ------------
