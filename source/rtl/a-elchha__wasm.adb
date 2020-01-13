cee3555f77860a2e555a4a372e875da98f8a011f
Vadim Godunko <vgodunko@gmail.com>
2020-01-14 00:11:20 +0300

    Remove full exception support, it is not supported by LLVM/WASL.

--- source/rtl/a-elchha__wasm.adb
+++ source/rtl/a-elchha__wasm.adb
@@ -1,12 +1,12 @@
 ------------------------------------------------------------------------------
 --                                                                          --
---                         GNAT COMPILER COMPONENTS                         --
+--                         GNAT RUN-TIME COMPONENTS                         --
 --                                                                          --
---                     S Y S T E M . T R A C E B A C K                      --
+--    A D A . E X C E P T I O N S . L A S T _ C H A N C E _ H A N D L E R   --
 --                                                                          --
---                                 S p e c                                  --
+--                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1999-2020, Free Software Foundation, Inc.         --
+--          Copyright (C) 2012-2020, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -25,34 +25,59 @@
 -- <http://www.gnu.org/licenses/>.                                          --
 --                                                                          --
 -- GNAT was originally developed  by the GNAT team at  New York University. --
--- It is now maintained by Ada Core Technologies Inc (http://www.gnat.com). --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  This package provides a method for generating a traceback of the current
---  execution location. The traceback shows the locations of calls in the call
---  chain, up to either the top or a designated number of levels.
+--  Default last chance handler for no propagation runtimes
 
---  This version is for the AE653 Level A run time and for bare board targets
+with Ada.Unchecked_Conversion;
 
-pragma Polling (Off);
---  We must turn polling off for this unit, because otherwise we get
---  elaboration circularities with System.Exception_Tables.
+with System.IO; use System.IO;
 
---  with System.Traceback_Entries;
+procedure Ada.Exceptions.Last_Chance_Handler
+  (Msg : System.Address; Line : Integer)
+is
+   procedure Put (Str : System.Address);
+   --  Put for a nul-terminated string (a C string)
 
-package body System.Traceback is
+   ---------
+   -- Put --
+   ---------
 
-   ----------------
-   -- Call_Chain --
-   ----------------
+   procedure Put (Str : System.Address) is
+      type C_String_Ptr is access String (1 .. Positive'Last);
+      function To_C_String_Ptr is new Ada.Unchecked_Conversion
+        (System.Address, C_String_Ptr);
 
-   procedure Call_Chain
-     (Traceback   : in out System.Traceback_Entries.Tracebacks_Array;
-      Max_Len     : Natural;
-      Len         : out Natural;
-      Exclude_Min : System.Address := System.Null_Address;
-      Exclude_Max : System.Address := System.Null_Address;
-      Skip_Frames : Natural := 1) is null;
+      Msg_Str : constant C_String_Ptr := To_C_String_Ptr (Str);
 
-end System.Traceback;
+   begin
+      for J in Msg_Str'Range loop
+         exit when Msg_Str (J) = Character'Val (0);
+         Put (Msg_Str (J));
+      end loop;
+   end Put;
+
+begin
+   Put_Line ("In last chance handler");
+
+   if Line /= 0 then
+      Put ("Predefined exception raised at ");
+      Put (Msg);
+      Put (':');
+      Put (Line);
+   else
+      Put ("User defined exception, message: ");
+      Put (Msg);
+   end if;
+
+   New_Line;
+
+   --  Stop the program
+
+   loop
+      null;
+   end loop;
+--   System.Machine_Reset.Stop;
+end Ada.Exceptions.Last_Chance_Handler;
--- source/rtl/s-soflin__wasm.ads
+++ source/rtl/s-soflin__wasm.ads
@@ -42,8 +42,8 @@
 pragma Compiler_Unit_Warning;
 pragma Style_Checks (Off);
 
-with Ada.Exceptions;
-with System.Exceptions.Machine;
+--  with Ada.Exceptions;
+--  with System.Exceptions.Machine;
 --  with System.Parameters;
 with System.Secondary_Stack;
 --  with System.Stack_Checking;
@@ -53,8 +53,8 @@ package System.Soft_Links is
 
    package SST renames System.Secondary_Stack;
 
-   subtype EOA is Ada.Exceptions.Exception_Occurrence_Access;
-   subtype EO is Ada.Exceptions.Exception_Occurrence;
+--   subtype EOA is Ada.Exceptions.Exception_Occurrence_Access;
+--   subtype EO is Ada.Exceptions.Exception_Occurrence;
 
 --   function Current_Target_Exception return EO;
 --   pragma Import
@@ -317,23 +317,13 @@ package System.Soft_Links is
 --   --  Ada.Exceptions.Exception_Data for a more detailed description of the
 --   --  potential problems.
 
-   procedure Save_Library_Occurrence (E : EOA);
+--   procedure Save_Library_Occurrence (E : EOA);
    --  When invoked, this routine saves an exception occurrence into a hidden
    --  reference. Subsequent calls will have no effect.
 
    ------------------------
    -- Task Specific Data --
    ------------------------
-
-   Max_Nbr_Occurrences : constant := 8;
-   --  Maximum number of in-flight (or nested) exceptions. An exception is
-   --  said in-flight when it is propagated or could be reraised.
-   --  XXX from certified profile. need to be removed
-
-   type Exceptions_Array is array (1 .. Max_Nbr_Occurrences) of
-     aliased System.Exceptions.Machine.GNAT_GCC_Exception;
-   --  Array for in-flight exceptions
-   --  XXX from certified profile. need to be removed
 
    --  Here we define a single type that encapsulates the various task
    --  specific data. This type is used to store the necessary data into the
@@ -363,15 +353,6 @@ package System.Soft_Links is
 --      --
 --      --  Also act as a list of the active exceptions in the case of the GCC
 --      --  exception mechanism, organized as a stack with the most recent first.
-
-      --  XXX from certified profile, need to be removed.
-
-      Last_Exception : Natural := 0;
-      --  Index of the last exception used in the array of exception. 0 means
-      --  no exception used.
-
-      Exceptions : Exceptions_Array;
-      --  Per thread static buffer of exceptions
    end record;
 
 --   procedure Create_TSD
@@ -429,137 +410,13 @@ package System.Soft_Links is
       Comp_3 : Boolean;
    end record;
 
---  XXX: Ravenscar / CERT
-
---  This package contains a set of subprogram access variables that access some
---  low-level primitives that are called different depending wether tasking is
---  involved or not (e.g. the Get/Set_Jmpbuf_Address that needs to provide a
---  different value for each task). To avoid dragging in the tasking all the
---  time, we use a system of soft links where the links are initialized to
---  non-tasking versions, and then if the tasking is initialized, they are
---  reset to the real tasking versions.
-
---pragma Compiler_Unit_Warning;
-
---with Ada.Exceptions;
---
---with System.Secondary_Stack;
---with System.Storage_Elements;
---with System.Exceptions.Machine;
-
---  package System.Soft_Links is
---     pragma Preelaborate;
-
---   package SST renames System.Secondary_Stack;
---
---   subtype EOA is Ada.Exceptions.Exception_Occurrence_Access;
---   subtype EO is Ada.Exceptions.Exception_Occurrence;
---
---   --  First we have the access subprogram types used to establish the links.
---   --  The approach is to establish variables containing access subprogram
---   --  values which by default point to dummy no tasking versions of routines.
---
---   type EO_Param_Proc     is access procedure (Excep : EO);
---
---   type Get_Stack_Call  is access function return SST.SS_Stack_Ptr;
---   type Set_Stack_Call  is access procedure (Stack : SST.SS_Stack_Ptr);
---
---   type Get_Integer_Call  is access function return Integer;
---   type Set_Integer_Call  is access procedure (Len : Integer);
---
---   type Get_EOA_Call      is access function return EOA;
---
---   --  Suppress checks on all these types, since we know corresponding values
---   --  can never be null (the soft links are always initialized).
---
---   pragma Suppress (Access_Check, Get_Stack_Call);
---   pragma Suppress (Access_Check, Set_Stack_Call);
---   pragma Suppress (Access_Check, Get_Integer_Call);
---   pragma Suppress (Access_Check, Set_Integer_Call);
---   pragma Suppress (Access_Check, Get_EOA_Call);
---
---   function  Get_Sec_Stack_NT return  SST.SS_Stack_Ptr;
---   procedure Set_Sec_Stack_NT (Stack : SST.SS_Stack_Ptr);
---
---   Get_Sec_Stack : Get_Stack_Call := Get_Sec_Stack_NT'Access;
---   Set_Sec_Stack : Set_Stack_Call := Set_Sec_Stack_NT'Access;
---
---   --  Export the Get/Set routines for the various Task Specific Data (TSD)
---   --  elements as callable subprograms instead of objects of access to
---   --  subprogram types.
---
---   function  Get_Sec_Stack_Soft return  SST.SS_Stack_Ptr;
---   procedure Set_Sec_Stack_Soft (Stack : SST.SS_Stack_Ptr);
---   pragma Inline (Get_Sec_Stack_Soft);
---   pragma Inline (Set_Sec_Stack_Soft);
---
---   function Get_GNAT_Exception return Ada.Exceptions.Exception_Id;
---   --  This function obtains the Exception_Id from Exception_Occurrence
---   --  referenced by the current exception of the task specific data.
---   --  This is used to transfer exceptions from entries.
---
---   type Stack_Info is record
---      Start_Address : System.Address := System.Null_Address;
---      Size          : System.Storage_Elements.Storage_Offset;
---   end record;
---   pragma Suppress_Initialization (Stack_Info);
---
---   Max_Nbr_Occurrences : constant := 8;
---   --  Maximum number of in-flight (or nested) exceptions. An exception is
---   --  said in-flight when it is propagated or could be reraised.
---
---   type Exceptions_Array is array (1 .. Max_Nbr_Occurrences) of
---     aliased System.Exceptions.Machine.GNAT_GCC_Exception;
---   --  Array for in-flight exceptions
-
---   type Sjlj_Function_Context is null record;
---   type Sjlj_Function_Context_Acc is access Sjlj_Function_Context;
---   pragma Convention (C, Sjlj_Function_Context_Acc);
---   --  Ada opaque representation of the SjLj function context pointer
-
---   type TSD is record
---      Pri_Stack_Info : aliased Stack_Info;
---      --  Information on stack (Base/Limit/Size) used by System.Stack_Checking.
---      --  If this TSD does not belong to the environment task, the Size field
---      --  must be initialized to the tasks requested stack size before the task
---      --  can do its first stack check.
---
---      Sec_Stack_Ptr : SST.SS_Stack_Ptr;
---      --  Pointer of the allocated secondary stack
---
---      Sjlj_Context : Sjlj_Function_Context_Acc := null;
---      --  Pointer to the function context for sjlj exception
---
---      Last_Exception : Natural := 0;
---      --  Index of the last exception used in the array of exception. 0 means
---      --  no exception used.
---
---      Exceptions : Exceptions_Array;
---      --  Per thread static buffer of exceptions
---   end record;
-
-   type TSD_Access is access all TSD;
-
-   type Get_TSD_Call is not null access function return TSD_Access;
-
-   function Get_TSD_NT return TSD_Access;
-
-   Get_TSD_Addr : Get_TSD_Call := Get_TSD_NT'Access;
-
---   procedure Set_Sjlj_Context (Fc : Sjlj_Function_Context_Acc);
---   pragma Export (C, Set_Sjlj_Context, "__gnat_set_sjlj_context");
---   function  Get_Sjlj_Context return Sjlj_Function_Context_Acc;
---   pragma Export (C, Get_Sjlj_Context, "__gnat_get_sjlj_context");
---   --  Routine (compatible with C) to set and get the sjlj function context
---   end System.Soft_Links;
-
---  XXX: END CERT
-
 private
+
    NT_TSD : aliased TSD;
    --  The task specific data for the main task when the Ada tasking run-time
    --  is not used. It relies on the default initialization of NT_TSD. It is
    --  placed here and not the body to ensure the default initialization does
    --  not clobber the secondary stack initialization that occurs as part of
    --  System.Soft_Links.Initialization.
+
 end System.Soft_Links;
