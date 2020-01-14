7d76f88fff64abec23c36f1212be0b7da55f3409
Vadim Godunko <vgodunko@gmail.com>
2020-01-14 21:50:17 +0300

    Getter for Element::hidden attribute.

--- source/html/web-html-elements.adb
+++ source/html/web-html-elements.adb
@@ -5,17 +5,34 @@ with WASM.Objects;
 
 package body Web.HTML.Elements is
 
-   procedure Set_Hidden (Element : WASM.Objects.Object_Identifier; To : Interfaces.Unsigned_32)
-     with Import    => True,
-          Link_Name => "__html_element_hidden_setter";
+   ----------------
+   -- Get_Hidden --
+   ----------------
+
+   function Get_Hidden (Self : HTML_Element'Class) return Boolean is
+      use type Interfaces.Unsigned_32;
+
+      function Imported
+       (Element : WASM.Objects.Object_Identifier)
+          return Interfaces.Unsigned_32
+            with Import    => True,
+                 Link_Name => "__adawebpack__html__Element__hidden_getter";
+   begin
+      return Imported (Self.Identifier) /= 0;
+   end Get_Hidden;
 
    ----------------
    -- Set_Hidden --
    ----------------
 
    procedure Set_Hidden (Self : in out HTML_Element'Class; To : Boolean) is
+      procedure Imported
+       (Element : WASM.Objects.Object_Identifier; To : Interfaces.Unsigned_32)
+          with Import    => True,
+               Link_Name => "__adawebpack__html__Element__hidden_setter";
+
    begin
-      Set_Hidden (Self.Identifier, Boolean'Pos (To));
+      Imported (Self.Identifier, Boolean'Pos (To));
    end Set_Hidden;
 
 end Web.HTML.Elements;
--- source/html/web-html-elements.ads
+++ source/html/web-html-elements.ads
@@ -5,6 +5,7 @@ package Web.HTML.Elements is
 
    type HTML_Element is new Web.DOM.Elements.Element with null record;
 
+   function Get_Hidden (Self : HTML_Element'Class) return Boolean;
    procedure Set_Hidden (Self : in out HTML_Element'Class; To : Boolean);
 
 end Web.HTML.Elements;
