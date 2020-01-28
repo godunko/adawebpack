32f5c113b398b2eec1d22b4b9ad4b73ca15db5be
Vadim Godunko <vgodunko@gmail.com>
2020-01-28 11:49:12 +0300

    Add simple heap memory manager.

--- source/rtl/s-memory__wasm.adb
+++ source/rtl/s-memory__wasm.adb
@@ -35,22 +35,45 @@ pragma Restrictions (No_Elaboration_Code);
 --  This unit may be linked without being with'ed, so we need to ensure
 --  there is no elaboration code (since this code might not be executed).
 
+with System.Address_To_Access_Conversions;
 with System.Storage_Elements;
 
 package body System.Memory is
+
    use System.Storage_Elements;
 
-   Heap_Base : Character;
-   for Heap_Base'Alignment use Standard'Maximum_Alignment;
-   pragma Import (C, Heap_Base, "__heap_base");
+   Metadata_Size : constant Storage_Count := 16;
+
+   type Metadata;
+
+   type Metadata_Access is access all Metadata;
+
+   pragma Warnings (Off);
+   type Metadata is record
+      Capacity : Storage_Count;
+      Next     : Metadata_Access;
+      Is_Free  : Boolean;
+   end record
+     with Size => Standard'Storage_Unit * Metadata_Size;
+   pragma Warnings (On);
+
+   package Metadata_Access_To_Address_Conversions is
+     new System.Address_To_Access_Conversions (Metadata);
+
+   function To_Address (Item : Metadata_Access) return System.Address
+     is (Metadata_Access_To_Address_Conversions.To_Address
+          (Metadata_Access_To_Address_Conversions.Object_Pointer (Item)));
+
+   function To_Access (Item : System.Address) return Metadata_Access
+     is (Metadata_Access
+          (Metadata_Access_To_Address_Conversions.To_Pointer (Item)));
+
+   Heap_Base : aliased Metadata
+     with Alignment  => Standard'Maximum_Alignment,
+          Import     => True,
+          Convention => C,
+          Link_Name  => "__heap_base";
    --  The address of the variable is the start of the heap
-
---   Heap_End : Character;
---   pragma Import (C, Heap_End, "__heap_end");
---   --  The address of the variable is the end of the heap
-
-   Top : aliased Address := Heap_Base'Address;
-   --  First not used address (always aligned to the maximum alignment).
 
    ----------------
    -- For C code --
@@ -73,7 +96,7 @@ package body System.Memory is
    function Alloc (Size : size_t) return System.Address is
       Max_Align : constant := Standard'Maximum_Alignment;
       Max_Size  : Storage_Count;
-      Res       : Address;
+      Res       : Address := Null_Address;
 
    begin
       if Size = 0 then
@@ -99,20 +122,45 @@ package body System.Memory is
            ((Storage_Count (Size) + Max_Align - 1) / Max_Align) * Max_Align;
       end if;
 
-      Res := Top;
+      if Heap_Base.Capacity = 0 then
+         --  First request, data structure is not initialized.
 
-      --  Detect too large allocation
+         Heap_Base := (Capacity => Max_Size, Next => null, Is_Free => False);
+         Res := Heap_Base'Address + Metadata_Size;
 
-      --  XXX Code is commented out because amount of memory is configurable
-      --  during linking/instantiation and can grow on request. This code
-      --  need to be revisited.
+      else
+         declare
+            Current : Metadata_Access := Heap_Base'Access;
+            Last    : Metadata_Access;
 
---      if Max_Size >= Storage_Count (Heap_End'Address - Res) then
---         raise Storage_Error;
---      end if;
+         begin
+            while Current /= null loop
+               if Current.Is_Free and Current.Capacity >= Max_Size then
+                  --  Reuse existing block.
 
-      --  Update the top of the heap.
-      Top := Res + Max_Size;
+                  Current.Is_Free := False;
+                  Res := To_Address (Current) + Metadata_Size;
+
+                  exit;
+               end if;
+
+               Last    := Current;
+               Current := Current.Next;
+            end loop;
+
+            if Res = Null_Address then
+               --  Allocate new block.
+
+               Current :=
+                 To_Access (To_Address (Last) + Metadata_Size + Last.Capacity);
+               Last.Next := Current;
+
+               Current.all :=
+                (Capacity => Max_Size, Next => null, Is_Free => False);
+               Res := To_Address (Current) + Metadata_Size;
+            end if;
+         end;
+      end if;
 
       return Res;
    end Alloc;
@@ -151,9 +199,10 @@ package body System.Memory is
    ----------
 
    procedure Free (Ptr : System.Address) is
-      pragma Unreferenced (Ptr);
+      Header : constant Metadata_Access := To_Access (Ptr - Metadata_Size);
+
    begin
-      null;
+      Header.Is_Free := True;
    end Free;
 
    -------------
