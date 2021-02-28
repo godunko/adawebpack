224a050c5a2251c0345d673b676d20a32604eb88
Vadim Godunko <vgodunko@gmail.com>
2021-02-28 18:38:54 +0300

    DOM::DocumentFragment binding

---
+++ source/dom/web-dom-document_fragments.adb
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                AdaWebPack                                --
+--                                                                          --
+------------------------------------------------------------------------------
+--  Copyright © 2021, Vadim Godunko                                         --
+--  All rights reserved.                                                    --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--                                                                          --
+--  1. Redistributions of source code must retain the above copyright       --
+--     notice, this list of conditions and the following disclaimer.        --
+--                                                                          --
+--  2. Redistributions in binary form must reproduce the above copyright    --
+--     notice, this list of conditions and the following disclaimer in the  --
+--     documentation and/or other materials provided with the distribution. --
+--                                                                          --
+--  3. Neither the name of the copyright holder nor the names of its        --
+--     contributors may be used to endorse or promote products derived      --
+--     from this software without specific prior written permission.        --
+--                                                                          --
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     --
+--  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       --
+--  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR   --
+--  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT    --
+--  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  --
+--  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        --
+--  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   --
+--  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   --
+--  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     --
+--  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   --
+--  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    --
+------------------------------------------------------------------------------
+
+with WASM.Methods;
+with WASM.Objects.Methods;
+
+package body Web.DOM.Document_Fragments is
+
+   -----------------------
+   -- Get_Element_By_Id --
+   -----------------------
+
+   overriding function Get_Element_By_Id
+    (Self       : Document_Fragment;
+     Element_Id : Web.Strings.Web_String) return Web.DOM.Elements.Element is
+   begin
+      return
+        Web.DOM.Elements.Instantiate
+         (WASM.Objects.Methods.Call_Object_String
+           (Self, WASM.Methods.Get_Element_By_Id, Element_Id));
+   end Get_Element_By_Id;
+
+end Web.DOM.Document_Fragments;
---
+++ source/dom/web-dom-document_fragments.ads
@@ -0,0 +1,70 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                            Web API Definition                            --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2021, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision: 5518 $ $Date: 2015-12-20 14:25:24 +0300 (Sun, 20 Dec 2015) $
+------------------------------------------------------------------------------
+--  This package provides binding to interface DocumentFragment.
+------------------------------------------------------------------------------
+
+with Web.DOM.Elements;
+with Web.DOM.Nodes;
+with Web.DOM.Non_Element_Parent_Nodes;
+with Web.DOM.Texts;
+with Web.Strings;
+
+package Web.DOM.Document_Fragments is
+
+   pragma Preelaborate;
+
+   type Document_Fragment is
+     new Web.DOM.Nodes.Node
+       and Web.DOM.Non_Element_Parent_Nodes.Non_Element_Parent_Node
+         with null record;
+
+--  interface DocumentFragment : Node {
+--    constructor();
+--  };
+
+   overriding function Get_Element_By_Id
+    (Self       : Document_Fragment;
+     Element_Id : Web.Strings.Web_String) return Web.DOM.Elements.Element;
+
+end Web.DOM.Document_Fragments;
